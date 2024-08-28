219cf0c1fc536a793fe038849f4286a9ea217e2f
Androff <rageclock23@gmail.com>
2024-08-28 23:36:07 +0200

    success

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min, this.max, this.current;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    (this.min = min),
+      (this.max = max),
+      (this.current = Math.ceil((this.max - this.min) / 2 + this.min));
+  }
 
-    }
+  guess() {
+    return this.current;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.current;
+    this.current = Math.ceil((this.max - this.min) / 2 + this.min);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.current;
+    this.current = Math.ceil((this.max - this.min) / 2 + this.min);
+  }
 }
 
 module.exports = GuessingGame;
